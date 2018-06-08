4cae8047c3a35bd8870b1c3205e2b69959684d9b
gregoiremeunier <gregoire.meunier@hotmail.fr>
2018-06-08 16:44:09 +0200

    main fonctions

--- compil.c
+++ compil.c
@@ -1,2 +1,14 @@
 //convert abstract machien code into model language
 //S::=x:=e | skip | S;S | if b then S else S | while b do S
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+parseProg()
+parseInstr()
+parseAtom()
